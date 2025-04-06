136f6eb296246fd079fe33fa934f5dd350e41553
DaviDAliagA25 <102503325+DaviDAliagA25@users.noreply.github.com>
2025-04-06 19:52:58 -0400

    Create Juego.java

---
+++ Practica-7/Juego-1/Juego.java
@@ -0,0 +1,28 @@
+package aplicacion;
+/*
+ * @author DAVID
+ */
+public class Juego {
+    public int numeroDeVidas;
+    public int record;
+
+    public Juego(int numeroDeVidas) {
+        this.numeroDeVidas = numeroDeVidas;
+        this.record = 0;
+    }
+
+    public void reiniciaPartida() {
+        System.out.println("Reiniciando partida...");
+    }
+
+    public void actualizaRecord() {
+        record++;
+        System.out.println("Nuevo record actualizado. Record actual: " + record);
+    }
+
+    public boolean quitaVida() {
+        numeroDeVidas--;
+        System.out.println("Te queda(n) " + numeroDeVidas + " vida(s).");
+        return numeroDeVidas > 0;
+    }
+}
