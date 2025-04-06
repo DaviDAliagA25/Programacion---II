296ed334db9f521cab13f4e43afe7cc72c88d80b
DaviDAliagA25 <102503325+DaviDAliagA25@users.noreply.github.com>
2025-04-06 19:49:10 -0400

    Create Aplicacion.java

---
+++ Practica-7/Juego-1/Aplicacion.java
@@ -0,0 +1,10 @@
+package aplicacion;
+/*
+ * @author DAVID
+ */
+public class Aplicacion { 
+    public static void main(String[] args) {
+        JuegoAdivinaNumero juego = new JuegoAdivinaNumero(3);
+        juego.juega();
+    }    
+}
