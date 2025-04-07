08aadc3ac80b1e803159982cbdf94597b598ccca
DaviDAliagA25 <102503325+DaviDAliagA25@users.noreply.github.com>
2025-04-06 20:05:49 -0400

    Create JuegoAdivinaNumero.java

---
+++ Practica-7/Juego-2/JuegoAdivinaNumero.java
@@ -0,0 +1,55 @@
+package aplicacion2;
+/*
+ * @author DAVID
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class JuegoAdivinaNumero extends Juego {
+    public int numeroAAdivinar;
+
+    public JuegoAdivinaNumero(int numeroDeVidas) {
+        super(numeroDeVidas);
+    }
+
+    public boolean validaNumero(int numero) {
+        return numero >= 0 && numero <= 10;
+    }
+
+    public void juega() {
+        reiniciaPartida();
+        Random rand = new Random();
+        numeroAAdivinar = rand.nextInt(11);
+
+        Scanner sc = new Scanner(System.in);
+        boolean sigueJugando = true;
+
+        while (sigueJugando) {
+            System.out.print("Adivina un numero entre 0 y 10: ");
+            int intento = sc.nextInt();
+
+            if (!validaNumero(intento)) {
+                System.out.println("Numero invalido. Debe estar entre 0 y 10.");
+                continue;
+            }
+
+            if (intento == numeroAAdivinar) {
+                System.out.println("Acertaste");
+                actualizaRecord();
+                sigueJugando = false;
+            } else {
+                System.out.println("Numero incorrecto.");
+                if (quitaVida()) {
+                    if (intento < numeroAAdivinar) {
+                        System.out.println("Pista: el numero es mayor.");
+                    } else {
+                        System.out.println("Pista: el numero es menor.");
+                    }
+                } else {
+                    System.out.println("No te quedan mas vidas. Fin del juego.");
+                    sigueJugando = false;
+                }
+            }
+        }
+    }
+}
